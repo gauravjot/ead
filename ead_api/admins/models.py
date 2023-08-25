d513f71f2734e6a29dcce0c03475c8c4a4088e5c
Gauravjot Garaya <gauravjot@outlook.com>
2023-08-25 14:56:45 -0700

    added models

--- ead_api/admins/models.py
+++ ead_api/admins/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Admin(models.Model):
+    id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=48)
+    title = models.CharField(max_length=48)
+    username = models.CharField(max_length=24)
+    password = models.CharField(max_length=96)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"id:{self.pk}, {self.full_name}, {self.username}"
+
+class Session(models.Model):
+    token = models.CharField(max_length=128)
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    valid = models.BooleanField(default=True)
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return f"User: {self.user}, Session made: {self.created} by {self.ip}, Valid: {self.valid}"
--- ead_api/users/models.py
+++ ead_api/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from admins.models import Admin
 
-# Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=48)
+    title = models.CharField(max_length=48)
+    email = models.EmailField(max_length=64)
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField()
+    created_by = models.ForeignKey(Admin, on_delete=models.SET_NULL, blank=True, null=True)
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"id:{self.pk}, {self.name}, {self.email}"
