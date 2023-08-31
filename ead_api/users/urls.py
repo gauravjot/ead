02027fda75a80b8d59c07b621d18f7b91f67c3b9
Gauravjot Garaya <gauravjot@outlook.com>
2023-08-31 10:18:41 -0700

    added views for users

--- ead_api/users/urls.py
+++ ead_api/users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import addUser, getUser, getAllUsers, searchUser, updateUser, disableUser, deleteUser 
+from .views import addUser, getUser, getAllUsers, searchUser, updateUser, deleteUser 
 
 urlpatterns = [
     path('api/user/add/', addUser),
     path('api/user/get/', getUser),
     path('api/user/all/', getAllUsers),
     path('api/user/search/', searchUser),
-    path('api/user/update/', updateUser),
-    path('api/user/disable/', disableUser),
-    path('api/user/delete/', deleteUser)
+    path('api/user/update/', updateUser), 
+    path('api/user/delete/', deleteUser),
 ]
--- ead_api/users/views.py
+++ ead_api/users/views.py
@@ -1,9 +1,14 @@
+import pytz
+import uuid
+from datetime import datetime
 from django.shortcuts import render
 # RestFramework
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from admins.sessions import getAdminID
+# Serializers
+from .serializers import UserSerializer
 
 
 # Create
@@ -11,44 +16,103 @@ from admins.sessions import getAdminID
 
 @api_view(['POST'])
 def addUser(request):
+    # Get requesting admin ID
     admin = getAdminID(request)
-    pass
+    if type(admin) is Response:
+       return admin
+    # Add user
+    dateStamp = datetime.now(pytz.utc)
+    userSerializer = UserSerializer(data=dict(
+        id=uuid.uuid4(),
+        name=str(request.data['name'])[0:48],
+        email=str(request.data['email'])[0:64].lower(),
+        phone=str(request.data['phone'])[0:20],
+        title=str(request.data['title'])[0:48],
+        created_at=dateStamp,
+        updated_at=dateStamp,
+        created_by=admin
+    ))
+
+    # -- check if data is without bad actors
+    if userSerializer.is_valid():
+        userSerializer.save()
+        return Response(data=successResponse({"user": userSerializer.data}), status=status.HTTP_201_CREATED)
+    else:
+        return Response(data=errorResponse(userSerializer.errors), status=status.HTTP_400_BAD_REQUEST)
+
 
 # Read
 # -------------------------------
 
 @api_view(['GET'])
 def getUser(request):
+    # Get requesting admin ID
     admin = getAdminID(request)
-    pass
+    if type(admin) is Response:
+        return admin
+    # Get user 
+    try:
+        user = User.objects.get(id=request['uid'])
+        return Response(data=successResponse(UserSerializer(user).data), status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response(data=errorResponse("User does not exist.", "U0001"), status=status.HTTP_404_NOT_FOUND)
+
 
 @api_view(['GET'])
 def getAllUsers(request):
+    # Get requesting admin ID
     admin = getAdminID(request)
-    pass
+    if type(admin) is Response:
+        return admin
+    return Response(data=successResponse(UserSerializer(User.objects.all().order_by('name'), many=True).data), status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 def searchUser(request):
+    # Get requesting admin ID
     admin = getAdminID(request)
-    pass
+    if type(admin) is Response:
+        return admin
+    return Response(data=successResponse(UserSerializer(User.objects.filter(name__icontains=str(request.data['keyword'])).order_by('name'), many=True).data), status=status.HTTP_200_OK)
+
 
 # Update
 # -------------------------------
 
 @api_view(['PUT'])
 def updateUser(request):
+    # Get requesting admin ID
     admin = getAdminID(request)
-    pass
+    if type(admin) is Response:
+        return admin
+    # Update
+    try:
+        user = User.objects.get(id=request.data['uid'])
+        user.name = str(request.data['name'])[0:48]
+        user.title = str(request.data['title'])[0:48]
+        user.email = str(request.data['email'])[0:64]
+        user.phone = str(request.data['phone'])[0:20]
+        user.updated_at = datetime.now(pytz.utc)
+        user.save()
+
+        return Response(data=successResponse(UserSerializer(user).data), status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response(data=errorResponse("Unable to update user.", "U0002"), status=status.HTTP_400_BAD_REQUEST)
+
+
 
 # Disable
 # -------------------------------
 
 @api_view(['DELETE'])
-def disableUser(request):
-    admin = getAdminID(request)
-    pass
-
-@api_view(['DELETE'])
 def deleteUser(request):
+    # Get requesting admin ID
     admin = getAdminID(request)
-    pass
+    if type(admin) is Response:
+        return admin
+    # Delete user
+    try:
+        User.objects.get(id=request.data['uid']).delete()
+        return Response(data=successResponse(), status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response(data=errorResponse("User does not exist.","N0003"), status=status.HTTP_404_NOT_FOUND)
